98ce4dc01eb93d23a0533c5ab55c70f1ea999505
andreasaresti <andreas.aresti@gmail.com>
2023-06-20 15:08:20 +0300

    ds

--- packages/Laravel-Pagebuilder/src/ServiceProvider.php
+++ packages/Laravel-Pagebuilder/src/ServiceProvider.php
@@ -30,27 +30,27 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         // $this->loadMigrationsFrom(__DIR__ . '/../migrations');
 
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                CreateTheme::class,
-                PublishTheme::class,
-                PublishDemo::class,
-            ]);
-        } elseif (empty(config('pagebuilder'))) {
-            throw new Exception("No PHPageBuilder config found, please run: php artisan vendor:publish --provider=\"Fosetico\LaravelPageBuilder\ServiceProvider\" --tag=config");
-        }
+        // if ($this->app->runningInConsole()) {
+        //     $this->commands([
+        //         CreateTheme::class,
+        //         PublishTheme::class,
+        //         PublishDemo::class,
+        //     ]);
+        // } elseif (empty(config('pagebuilder'))) {
+        //     throw new Exception("No PHPageBuilder config found, please run: php artisan vendor:publish --provider=\"Fosetico\LaravelPageBuilder\ServiceProvider\" --tag=config");
+        // }
 
-        // register singleton phpPageBuilder (this ensures phpb_ helpers have the right config without first manually creating a PHPageBuilder instance)
-        $this->app->singleton('phpPageBuilder', function($app) {
-            return new PHPageBuilder(config('pagebuilder') ?? []);
-        });
-        $this->app->make('phpPageBuilder');
+        // // register singleton phpPageBuilder (this ensures phpb_ helpers have the right config without first manually creating a PHPageBuilder instance)
+        // $this->app->singleton('phpPageBuilder', function($app) {
+        //     return new PHPageBuilder(config('pagebuilder') ?? []);
+        // });
+        // $this->app->make('phpPageBuilder');
 
-        $this->publishes([
-            __DIR__ . '/../config/pagebuilder.php' => config_path('pagebuilder.php'),
-        ], 'config');
-        $this->publishes([
-            __DIR__ . '/../themes/demo' => base_path(config('pagebuilder.theme.folder_url') . '/demo'),
-        ], 'demo-theme');
+        // $this->publishes([
+        //     __DIR__ . '/../config/pagebuilder.php' => config_path('pagebuilder.php'),
+        // ], 'config');
+        // $this->publishes([
+        //     __DIR__ . '/../themes/demo' => base_path(config('pagebuilder.theme.folder_url') . '/demo'),
+        // ], 'demo-theme');
     }
 }
