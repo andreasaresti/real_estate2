79b65bce4316af2cad4536b822afb0c4dfea8b0e
andreasaresti <andreas.aresti@gmail.com>
2023-06-20 14:46:41 +0300

    remove autoload dumb

--- packages/PHPageBuilder/src/Core/DB.php
+++ packages/PHPageBuilder/src/Core/DB.php
@@ -25,13 +25,13 @@ class DB
      */
     public function __construct(array $config)
     {
-        // $this->pdo = new PDO(
-        //     $config['driver'] . ':host=' . $config['host'] . ';dbname=' . $config['database'] . ";options='--client_encoding=" . $config['charset'] . "'",
-        //     $config['username'],
-        //     $config['password'],
-        //     [PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT]
-        // );
-        // $this->pdo->exec("set names " . $config['charset']);
+        $this->pdo = new PDO(
+            $config['driver'] . ':host=' . $config['host'] . ';dbname=' . $config['database'] . ";options='--client_encoding=" . $config['charset'] . "'",
+            $config['username'],
+            $config['password'],
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT]
+        );
+        $this->pdo->exec("set names " . $config['charset']);
     }
 
     /**
