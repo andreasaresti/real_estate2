43896d237568a41104537232a0341d1eedb46431
Tho Nguyen <thonguyen.freelance@gmail.com>
2023-07-03 18:14:45 +0300

    Add the "add_note" class

--- app/Http/Controllers/webSalesRequestController.php
+++ app/Http/Controllers/webSalesRequestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SalesRequestNote;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
@@ -68,6 +69,28 @@ class webSalesRequestController extends Controller
     }
     public function add_note(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'sales_request_id' => 'required|integer|exists:sales_requests,id|unique:sales_request_notes',
+            'sales_request_note_type_id' => 'required|integer|exists:sales_request_note_types,id|unique:sales_request_notes',
+            'description' => 'required | string'
+        ]);
+        if ($validator->fails()) {
+            // Return the validation errors
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $note = SalesRequestNote::create([
+            'sales_request_id' => $request->sales_request_id,
+            'sales_request_note_type_id' => $request->sales_request_note_type_id,
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            'message' => 'Note created successfully',
+            'note' => $note,
+        ], 201);
     }
     public function get_notes(Request $request)
     {
