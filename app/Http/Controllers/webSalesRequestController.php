27c6e84431f962bf137f256c2fb0ba8566c13a63
Tho Nguyen <thonguyen.freelance@gmail.com>
2023-07-03 17:43:44 +0300

    Add the "close_deal" class in webSalesRequestController.php

--- app/Http/Controllers/webSalesRequestController.php
+++ app/Http/Controllers/webSalesRequestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\DB;
 
 class webSalesRequestController extends Controller
 {
@@ -14,7 +15,7 @@ class webSalesRequestController extends Controller
         $validator = Validator::make($request->all(), [
             'id' => 'required|integer',
             'status' => 'required|in:won,lost',
-            'listing_id' => 'nullable|integer|required_if:status,won|exists:listings,id',
+            'listing_id' => 'nullable|integer|required_if:status,won|exists:sales_request_listings,id',
             'agreement_price' => 'nullable|integer|required_if:status,won',
             'sales_lost_reason_id' => 'nullable|integer|required_if:status,lost|exists:sales_lost_reasons,id',
         ]);
@@ -25,12 +26,89 @@ class webSalesRequestController extends Controller
                 'errors' => $validator->errors(),
             ], 422);
         }
+
+        $perPage = 20;
+        $page = 1;
+        $orderby = 'sales_requests.id';
+        $orderbytype = 'desc';
+
+        $query = DB::table('sales_requests');
+
+        if (
+            $request->has('id') &&
+            $request->id != '' &&
+            $request->has('status') &&
+            $request->status != '' &&
+            $request->has('listing_id') &&
+            $request->listing_id != '' &&
+            $request->has('agreement_price') &&
+            $request->agreement_price != '' &&
+            $request->has('sales_lost_reason_id') &&
+            $request->sales_lost_reason_id != ''
+        ) {
+            // if ($request->status == "won") {
+            $query = $query
+                ->where('id', $request->id)
+                ->where('listing_id', $request->listing_id)
+                ->where('agreement_price', $request->agreement_price)
+                ->where('sales_lost_reason_id', $request->sales_lost_reason_id);
+        }
+
+        if ($query) {
+            $query = $query
+                ->select('sales_requests.*')
+                ->orderBy($orderby, $orderbytype)
+                ->paginate($perPage, ['/*'], 'page', $page);
+        } else {
+            $query = "No data";
+        }
+
+        $sales_requests = $query;
+        return response()->json($sales_requests);
     }
     public function add_note(Request $request)
     {
     }
     public function get_notes(Request $request)
     {
+        $perPage = 20;
+        $page = 1;
+        $orderby = 'sales_request_notes.id';
+        $orderbytype = 'desc';
+
+        $validator = Validator::make($request->all(), [
+            'sales_request_id' => 'required'
+        ]);
+
+        // Check if the validation fails
+        if ($validator->fails()) {
+            // Return the validation errors
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $query = DB::table('sales_request_notes');
+
+        if (
+            $request->has('sales_request_id') &&
+            $request->sales_request_id != '' &&
+            $request->has('sales_request_note_type_id') &&
+            $request->sales_request_note_type_id != ''
+        ) {
+            $query = $query
+                ->where('sales_request_id', $request->sales_request_id)
+                ->where('sales_request_note_type_id', $request->sales_request_note_type_id);
+        }
+
+        $query = $query
+            ->select('sales_request_notes.*')
+            ->orderBy($orderby, $orderbytype)
+            ->paginate($perPage, ['/*'], 'page', $page);
+
+        $sales_request_notes = $query;
+
+        return response()->json($sales_request_notes);
     }
     public function add_appointments(Request $request)
     {
@@ -62,6 +140,4 @@ class webSalesRequestController extends Controller
     public function update_sales_request(Request $request)
     {
     }
-
-    
 }
