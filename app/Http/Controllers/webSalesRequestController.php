7bd94001161021998fddbec0bb63c41b48280ec5
andreasaresti <andreas.aresti@gmail.com>
2023-07-04 14:08:07 +0300

    allow to not accept status

--- app/Http/Controllers/webSalesRequestController.php
+++ app/Http/Controllers/webSalesRequestController.php
@@ -298,6 +298,7 @@ class webSalesRequestController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'id' => 'required|integer|exists:sales_requests,id',
+            'accepted_status' => 'nullable|in:yes,no',
         ]);
         // Check if the validation fails
         if ($validator->fails()) {
@@ -306,9 +307,13 @@ class webSalesRequestController extends Controller
                 'errors' => $validator->errors(),
             ], 422);
         }
+        $accepted_status = 'yes';
+        if ($request->has('accepted_status') && $request->accepted_status != '') {
+            $accepted_status = $request->accepted_status;
+        }
 
         SalesRequest::where('id', $request->id)->update([
-            'accepted_status' => 'yes',
+            'accepted_status' => $accepted_status,
         ]);
 
         return response()->json([
