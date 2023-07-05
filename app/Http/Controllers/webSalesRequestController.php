2a8230e502399980da19deb4ab37af6fc9dfd1c8
andreasaresti <andreas.aresti@gmail.com>
2023-07-05 08:51:58 +0300

    update sales_request_listings

--- app/Http/Controllers/webSalesRequestController.php
+++ app/Http/Controllers/webSalesRequestController.php
@@ -269,6 +269,25 @@ class webSalesRequestController extends Controller
     }
     public function change_listing_type(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|integer|exists:sales_request_listings,id',
+            'status' => 'required|string|in:open,not_interested',
+        ]);
+        // Check if the validation errors
+        if ($validator->fails()) {
+            // Return the validation errors
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        SalesRequestListing::where('id', $request->id)->update([
+            'status' => $request->status,
+        ]);
+
+        return response()->json([
+            'message' => 'Sales Request Listings updated successfully'
+        ], 201);
     }
     // public function add_sales_request(Request $request)
     // {
