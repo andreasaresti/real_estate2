1a0953c5f84c51bf682c8d36ea076ecb58f28b2b
andreasaresti <andreas.aresti@gmail.com>
2023-07-06 07:22:35 +0300

    add create_listing in apis

--- app/Http/Controllers/webListings.php
+++ app/Http/Controllers/webListings.php
@@ -96,88 +96,93 @@ class webListings extends Controller
     }
     public function get_similar_listings(Request $request){
     }
-    // public function create_listing(Request $request)
-    // {
-    //     $validator = Validator::make($request->all(), [
-    //         'name' => 'required|string',
-    //         'image' => 'required|string',
-    //         'description' => 'required|string',
-    //         'price' => 'required|integer',
-    //         'map' => 'required|string',
-    //         'price_prefix' => 'required|string',
-    //         'price_postfix' => 'required|string',
-    //         'area_size' => 'required|integer',
-    //         'area_size_prefix' => 'required|string',
-    //         'area_size_postfix' => 'required|string',
-    //         'number_of_bedrooms' => 'required|integer',
-    //         'number_of_bathrooms' => 'required|integer',
-    //         'number_of_garages_or_parkingpaces' => 'required|integer',
-    //         'year_built' => 'required|integer',
-    //         'published' => 'required|integer',
-    //         'address' => 'required|string',
-    //         'latitude' => 'required|string',
-    //         'longitude' => 'required|string',
-    //         'virtual_tour' => 'required|string',
-    //         'energy_class' => 'required|string',
-    //         'energy_performance' => 'required|string',
-    //         'epc_current_rating' => 'required|string',
-    //         'epc_potential_rating' => 'required|string',
-    //         'location_id' => 'required|integer|exists:locations,id',
-    //         'property_type_id' => 'required|integer|exists:property_types,id',
-    //         'delivery_time_id' => 'required|integer|exists:delivery_times,id',
-    //         'owner_id' => 'required|integer|exists:customers,id',
-    //         'listing_listing_type' => 'nullable|array',
-    //         'images' => 'nullable|array',
-    //         'features' => 'nullable|array',
-    //     ]);
-    //     if ($validator->fails()) {
-    //         // Return the validation errors
-    //         return response()->json([
-    //             'errors' => $validator->errors(),
-    //         ], 422);
-    //     }
+    public function create_listing(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'image' => 'required|string',
+            'description' => 'required|string',
+            'price' => 'required|integer',
+            'map' => 'nullable|string',
+            'price_prefix' => 'nullable|string',
+            'price_postfix' => 'nullable|string',
+            'area_size' => 'required|integer',
+            'area_size_prefix' => 'nullable|string',
+            'area_size_postfix' => 'nullable|string',
+            'number_of_bedrooms' => 'nullable|integer',
+            'number_of_bathrooms' => 'nullable|integer',
+            'number_of_garages_or_parkingpaces' => 'nullable|integer',
+            'year_built' => 'nullable|integer',
+            'address' => 'required|string',
+            'latitude' => 'required|string',
+            'longitude' => 'required|string',
+            'virtual_tour' => 'nullable|string',
+            'energy_class' => 'nullable|string',
+            'energy_performance' => 'nullable|string',
+            'epc_current_rating' => 'nullable|string',
+            'epc_potential_rating' => 'nullable|string',
+            'location_id' => 'required|integer|exists:locations,id',
+            'property_type_id' => 'required|integer|exists:property_types,id',
+            'delivery_time_id' => 'required|integer|exists:delivery_times,id',
+            'owner_id' => 'required|integer|exists:customers,id',
+            'listing_listing_type' => 'nullable|array',
+            'images' => 'nullable|array',
+            'features' => 'nullable|array',
+            'listing_listing_type.*' => 'exists:listing_types,id',
+            'features.*' => 'exists:features,id',
+        ]);
+        if ($validator->fails()) {
+            // Return the validation errors
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
 
-    //     $listing = Listing::create([
-    //         'name' => $request->name,
-    //         'image' => $request->image,
-    //         'description' => $request->description,
-    //         'price' => $request->price,
-    //         'map' => $request->map,
-    //         'price_prefix' => $request->price_prefix,
-    //         'price_postfix' => $request->price_postfix,
-    //         'area_size' => $request->area_size,
-    //         'area_size_prefix' => $request->area_size_prefix,
-    //         'area_size_postfix' => $request->area_size_postfix,
-    //         'number_of_bedrooms' => $request->number_of_bedrooms,
-    //         'number_of_bashrooms' => $request->number_of_bashrooms,
-    //         'number_of_garages_or_parkingpaces' => $request->number_of_garages_or_parkingpaces,
-    //         'year_built' => $request->year_built,
-    //         'published' => $request->published,
-    //         'address' => $request->address,
-    //         'latitude' => $request->latitude,
-    //         'longitude' => $request->longitude,
-    //         '360_virtual_tour' => $request->virtual_tour,
-    //         'energy_class' => $request->energy_class,
-    //         'energy_performance' => $request->energy_performance,
-    //         'epc_current_rating' => $request->epc_current_rating,
-    //         'epc_potential_rating' => $request->epc_potential_rating,
-    //         'location_id' => $request->location_id,
-    //         'property_type_id' => $request->property_type_id,
-    //         'delivery_time_id' => $request->delivery_time_id,
-    //         'owner_id' => $request->owner_id,
-    //     ]);
+        $image = $request->image;
+        $image_parts = explode(";base64", $image);
+        $image_type_aux = explode('image/', $image_parts[0]);
+        $image_type = $image_type_aux[1];
+        $file = base64_decode($image_parts[1]);
+        $safeName = 'listing_' . time() . "." . $image_type;
+        file_put_contents(public_path('storage') . '/' . $safeName, $file);
+        
+        $listing = Listing::create([
+            'name' => $request->name,
+            'image' => $safeName,
+            'description' => $request->description,
+            'price' => $request->price,
+            'map' => $request->map,
+            'price_prefix' => $request->price_prefix,
+            'price_postfix' => $request->price_postfix,
+            'area_size' => $request->area_size,
+            'area_size_prefix' => $request->area_size_prefix,
+            'area_size_postfix' => $request->area_size_postfix,
+            'number_of_bedrooms' => $request->number_of_bedrooms,
+            'number_of_bashrooms' => $request->number_of_bashrooms,
+            'number_of_garages_or_parkingpaces' => $request->number_of_garages_or_parkingpaces,
+            'year_built' => $request->year_built,
+            'published' => 0,
+            'address' => $request->address,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+            '360_virtual_tour' => $request->virtual_tour,
+            'energy_class' => $request->energy_class,
+            'energy_performance' => $request->energy_performance,
+            'epc_current_rating' => $request->epc_current_rating,
+            'epc_potential_rating' => $request->epc_potential_rating,
+            'location_id' => $request->location_id,
+            'property_type_id' => $request->property_type_id,
+            'delivery_time_id' => $request->delivery_time_id,
+            'owner_id' => $request->owner_id,
+        ]);
 
-    //     if ($request->has('listing_listing_type') && count($request->listing_listing_type) > 0) {
-    //         foreach($request->listing_listing_type as $listing_type_id) {
+        
 
-    //         }
-    //     }
-
-    //         return response()->json([
-    //             'message' => 'Listings added successfully',
-    //             'listing' => $listing,
-    //         ], 201);
-    // }
+        return response()->json([
+            'message' => 'Listings added successfully',
+            'listing' => $listing,
+        ], 201);
+    }
     public function get_countries(Request $request)
     {
         $query = Country::select('countries.*')
