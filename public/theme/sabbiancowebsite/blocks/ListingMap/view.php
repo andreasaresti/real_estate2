15f76699c2a3cb9075aca5188b1b3b7317320a57
gtiger117 <104471486+gtiger117@users.noreply.github.com>
2023-08-30 00:11:57 +0900

    update circle size

--- public/theme/sabbiancowebsite/blocks/ListingMap/view.php
+++ public/theme/sabbiancowebsite/blocks/ListingMap/view.php
@@ -39,8 +39,8 @@
     <div class="container-fluid">
         <div class="row">
             <aside class="col-lg-6 col-md-6 google-maps-left mt-0">
-                <div class="row" style="width:92%;display: flex;align-items: center;margin: 25px 0px 0px 50px; position: absolute;z-index: 9;">
-                    <div class="col-xl-5 xsRow" style="display: flex;justify-content: space-around;align-items: center;padding: 0px;">
+                <div class="row" style="width:82%;display: flex;align-items: center;margin: 25px 0px 0px 50px; position: absolute;z-index: 9;">
+                    <div class="col-xl-12 xsRow" style="display: flex;justify-content: space-around;align-items: center;padding: 0px;">
                         <!-- <a  class="btn btn-map" id="mapSizeListingMap1" onclick="mapSizeListingMap(1);" style="margin-right:5px;">+ 1 km</a> -->
                         <a  class="btn btn-map" id="mapSizeListingMap5" onclick="mapSizeListingMap(5);" style="margin-right:5px;">+ 5 km</a>
                         <a  class="btn btn-map" id="mapSizeListingMap10" onclick="mapSizeListingMap(10);" style="margin-right:5px;">+ 10 km</a>
@@ -315,11 +315,11 @@
     }
 	// window.addEventListener("load", (event) => {
 
-        <?php
-if($selDistricts != ''){
-    echo "$( '#districts".$selDistricts."' ).click();";
-}
-        ?>
+    <?php
+        if($selDistricts != ''){
+            echo "$( '#districts".$selDistricts."' ).click();";
+        }
+    ?>
     
     
     loadActiveListingsListingMap([0,0],0);
